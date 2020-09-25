7f9c0f7946adb11154b01d25d1e94957150395f2
Luisa Barros <qlullyq@gmail.com>
2020-09-24 22:10:12 -0300

    feat: configure routes

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,9 @@ import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'Hello World' }));
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Server started: 3333');
