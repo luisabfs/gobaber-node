21d008e1e5ff636d34de9c786e9dc3bde567c7bc
Luisa Barros <qlullyq@gmail.com>
2020-10-14 15:19:16 -0300

    refactor: create auth config file

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '137aedd982d7fe7ef59d56eff294b1e3',
+    expiresIn: '1d',
+  },
+};
--- src/services/CreateSessionService.ts
+++ src/services/CreateSessionService.ts
@@ -2,6 +2,7 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
+import authConfig from '../config/auth';
 import User from '../models/User';
 
 interface Request {
@@ -32,9 +33,11 @@ class CreateSessionService {
       throw new Error('Incorrect email/password combination.');
     }
 
-    const token = sign({}, '137aedd982d7fe7ef59d56eff294b1e3', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
