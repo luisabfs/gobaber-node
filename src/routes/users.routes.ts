bff368c5a08a74782b5602844e465527656841fb
Luisa Barros <qlullyq@gmail.com>
2020-10-14 18:44:56 -0300

    feat: configure file upload

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
 import CreateUserService from '../services/CreateUserService';
 
+import ensureAuthentication from '../middlewares/auth';
+
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -23,5 +29,14 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthentication,
+  upload.single('avatar'),
+  async (request, response) => {
+    return response.json({ message: 'ok' });
+  },
+);
 
 export default usersRouter;
