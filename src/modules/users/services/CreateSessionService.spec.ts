456ffe9d8e86d3670351b74a01944fa1d94a63ab
Luísa Barros <qlullyq@gmail.com>
2020-12-02 00:28:25 -0300

    test: add non existing user authentication

--- src/modules/users/services/CreateSessionService.spec.ts
+++ src/modules/users/services/CreateSessionService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 
@@ -32,5 +34,22 @@ describe('AuthenticateUser', () => {
 
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(user);
+  });
+
+  it('should not be able to authenticate with non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createSession = new CreateSessionService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    expect(
+      createSession.execute({
+        email: 'jane@gmail.com',
+        password: '1234',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
