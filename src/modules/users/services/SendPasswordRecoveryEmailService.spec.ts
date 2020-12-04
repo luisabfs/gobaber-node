a05ff6e30970fce68bf4debf2f5c2ed0bdcfb691
Luísa Barros <qlullyq@gmail.com>
2020-12-04 15:50:57 -0300

    test: add password recovery token unity test

--- src/modules/users/services/SendPasswordRecoveryEmailService.spec.ts
+++ src/modules/users/services/SendPasswordRecoveryEmailService.spec.ts
@@ -2,14 +2,29 @@ import AppError from '@shared/errors/AppError';
 
 import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeUserTokensRepository from '../repositories/fakes/FakeUserTokensRepository';
 
 import SendPasswordRecoveryEmailService from './SendPasswordRecoveryEmailService';
 
-describe('SendPasswordRecoveryEmail', () => {
-  it('should be able to send a recovery password email', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeMailProvider = new FakeMailProvider();
+let fakeMailProvider: FakeMailProvider;
+let fakeUsersRepository: FakeUsersRepository;
+let fakeUserTokensRepository: FakeUserTokensRepository;
+let sendPasswordRecoveryEmail: SendPasswordRecoveryEmailService;
 
+describe('SendPasswordRecoveryEmail', () => {
+  beforeEach(() => {
+    fakeMailProvider = new FakeMailProvider();
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeUserTokensRepository = new FakeUserTokensRepository();
+
+    sendPasswordRecoveryEmail = new SendPasswordRecoveryEmailService(
+      fakeUsersRepository,
+      fakeMailProvider,
+      fakeUserTokensRepository,
+    );
+  });
+
+  it('should be able to send a recovery password email', async () => {
     const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
 
     await fakeUsersRepository.create({
@@ -17,11 +32,6 @@ describe('SendPasswordRecoveryEmail', () => {
       email: 'jane@gmail.com',
       password: '1234',
     });
-
-    const sendPasswordRecoveryEmail = new SendPasswordRecoveryEmailService(
-      fakeUsersRepository,
-      fakeMailProvider,
-    );
 
     await sendPasswordRecoveryEmail.execute({
       email: 'jane@gmail.com',
@@ -31,18 +41,26 @@ describe('SendPasswordRecoveryEmail', () => {
   });
 
   it('should not be able to recover password of a non-existing user', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeMailProvider = new FakeMailProvider();
-
-    const sendPasswordRecoveryEmail = new SendPasswordRecoveryEmailService(
-      fakeUsersRepository,
-      fakeMailProvider,
-    );
-
     await expect(
       sendPasswordRecoveryEmail.execute({
         email: 'jane@gmail.com',
       }),
     ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should generate a password recovery token', async () => {
+    const generateToken = jest.spyOn(fakeUserTokensRepository, 'generate');
+
+    const user = await fakeUsersRepository.create({
+      name: 'Jane Doe',
+      email: 'jane@gmail.com',
+      password: '1234',
+    });
+
+    await sendPasswordRecoveryEmail.execute({
+      email: 'jane@gmail.com',
+    });
+
+    expect(generateToken).toHaveBeenCalledWith(user.id);
   });
 });
