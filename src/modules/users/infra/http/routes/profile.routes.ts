a323d9821205e3dd13969e10bcf4c8efbe17f5df
Luísa Barros <qlullyq@gmail.com>
2020-12-08 19:51:04 -0300

    feat: create profile route

---
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+
+import ensureAuthentication from '../middlewares/auth';
+import ProfileController from '../controllers/ProfileController';
+
+const profileRouter = Router();
+const profileController = new ProfileController();
+
+profileRouter.use(ensureAuthentication);
+
+profileRouter.put('/', profileController.update);
+
+export default profileRouter;
