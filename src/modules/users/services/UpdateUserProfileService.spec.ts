11842ba02f319ed968c4168837bf47f52df3ce96
Luísa Barros <qlullyq@gmail.com>
2020-12-08 16:44:28 -0300

    test: add email conflict rejection to update user profile unity test

--- src/modules/users/services/UpdateUserProfileService.spec.ts
+++ src/modules/users/services/UpdateUserProfileService.spec.ts
@@ -1,4 +1,4 @@
-// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
@@ -35,5 +35,27 @@ describe('UpdateUserProfile', () => {
 
     expect(updatedUser.name).toBe('John Doe');
     expect(updatedUser.email).toBe('john@gmail.com');
+  });
+
+  it('should not be able to update to an email already in use', async () => {
+    await fakeUsersRepository.create({
+      name: 'Jane Doe',
+      email: 'jane@gmail.com',
+      password: '1234',
+    });
+
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'john@gmail.com',
+      password: '1234',
+    });
+
+    await expect(
+      updateUserProfile.execute({
+        user_id: user.id,
+        name: 'Jane Doe',
+        email: 'jane@gmail.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
