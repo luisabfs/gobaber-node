2e5c05b7083b0672f0c37c3f54da1249f6c99728
Luísa Barros <qlullyq@gmail.com>
2020-12-09 18:00:04 -0300

    feat: create list providers route

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -8,15 +8,6 @@ const appointmentsController = new AppointmentsController();
 
 appointmentsRouter.use(ensureAuthentication);
 
-// appointmentsRouter.get('/', async (request, response) => {
-//   const appointmentsRepository = new AppointmentsRepository();
-//   const appointments = await appointmentsRepository.find(
-//     appointmentsRepository,
-//   );
-
-//   return response.json(appointments);
-// });
-
 appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;
