e0ccf616d64f3567d107f40d53e10c08031f0746
Luísa Barros <qlullyq@gmail.com>
2020-12-13 13:04:59 -0300

    feat: add celebrate errors validations to server file

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import { errors } from 'celebrate';
+
 import 'express-async-errors';
 import cors from 'cors';
 
@@ -18,6 +20,7 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
+app.use(errors());
 app.use(
   (error: Error, request: Request, response: Response, _: NextFunction) => {
     if (error instanceof AppError) {
